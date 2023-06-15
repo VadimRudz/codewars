f66a360ea676629c51f8acc1f1128bf0cff4ec25
VadimRudz <vadimrudz@gmail.com>
2023-06-15 23:41:48 +0300

    feat: add task amountOfPages

---
+++ tasks/amountOfPages.js
@@ -0,0 +1,11 @@
+function amountOfPages (summary){
+  let accNumber = '';
+  let returnValue; 
+  for (let i = 1; accNumber.length < summary; i++) {
+    accNumber = accNumber + i;
+    returnValue = i;
+  }
+  return returnValue;
+}
+
+console.log(amountOfPages(5));
