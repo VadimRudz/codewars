155cc6b746cf34ec6e5c7905233d0d7abccd8c50
VadzimRudz <vadzim_rudz@senla.eu>
2024-05-10 13:07:46 +0200

    change approach

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,8 @@
 module.exports = {
     "env": {
         "browser": true,
-        "es2021": true
+        "es2021": true,
+        "mocha": true
     },
     "extends": "eslint:recommended",
     "overrides": [
@@ -29,5 +30,9 @@ module.exports = {
         "eol-last": ["error", "always"],
         "no-unused-vars": "off",
         "no-constant-condition": "off",
+    },
+    "globals": {
+        "describe": "readonly",
+        "it": "readonly"
     }
 };
---
+++ test/example.test.js
@@ -0,0 +1,12 @@
+// test/example.test.js
+import { equal } from 'assert';
+
+describe('Example', function() {
+    it('should pass', function() {
+        equal(1 + 1, 2);
+    });
+
+    it('should fail', function() {
+        equal(1 + 1, 3);
+    });
+});
