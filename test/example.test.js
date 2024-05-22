ea84efb3ecf883ec566d9e41bbb9ac8f2e617234
VadzimRudz <vadzim_rudz@senla.eu>
2024-05-22 10:59:38 +0200

    remove failed autotest

--- test/example.test.js
+++ test/example.test.js
@@ -5,8 +5,4 @@ describe('Example', function() {
     it('should pass', function() {
         equal(1 + 1, 2);
     });
-
-    // it('should fail', function() {
-    //     equal(1 + 1, 3);
-    // });
 });
