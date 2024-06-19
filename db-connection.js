e9cd5000d6228ddcba0bc521b784b9641194566f
VadzimRudz <vadzim_rudz@senla.eu>
2024-06-19 16:56:56 +0200

    add string fix

--- db-connection.js
+++ db-connection.js
@@ -5,7 +5,7 @@ import sql from 'mssql';
 const config = {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    server: process.env.DB_SERVER,
+    server: String(process.env.DB_SERVER),
     database: process.env.DB_NAME,
     options: {
         encrypt: true, // If we use encryption
