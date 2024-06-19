573ab120e82df07d916de444710809f7fc1c14cb
VadzimRudz <vadzim_rudz@senla.eu>
2024-06-19 17:01:15 +0200

    debugging

--- db-connection.js
+++ db-connection.js
@@ -3,16 +3,21 @@ import 'dotenv/config';
 import sql from 'mssql';
 
 const config = {
-    user: String(process.env.DB_USER),
-    password: String(process.env.DB_PASSWORD),
-    server: String(process.env.DB_SERVER),
-    database: String(process.env.DB_NAME),
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    server: process.env.DB_SERVER,
+    database: process.env.DB_NAME,
     options: {
         encrypt: true, // If we use encryption
         enableArithAbort: true,
         trustServerCertificate: true // Ignore server certificate
     }
 };
+
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
+console.log('DB_SERVER:', process.env.DB_SERVER);
+console.log('DB_NAME:', process.env.DB_NAME);
 
 const connectToDatabase = async () => {
     try {
